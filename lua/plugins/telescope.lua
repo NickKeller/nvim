351ca8f02bdbde0a5db618a17a4d2941acd58203
nick <keller.nicholas@outlook.com>
2024-09-15 17:59:31 -0400

    Change telescope keys to use C-n and C-p instead of C-j and C-k

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -18,8 +18,8 @@ return {
                 defaults = {
                     mappings = {
                         i = {
-                            ["<C-k>"] = actions.move_selection_previous,
-                            ["<C-j>"] = actions.move_selection_next,
+                            ["<C-p>"] = actions.move_selection_previous,
+                            ["<C-n>"] = actions.move_selection_next,
                             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
                             ["<C-x>"] = actions.delete_buffer,
                         },
